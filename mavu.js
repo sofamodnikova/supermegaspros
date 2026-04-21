f616d14b84d6640273e6866cc7be48aa954a9c00
sofamodnikova <smodnikova1604@gmail.com>
2026-04-21 13:10:49 +0300

    Add files via upload

---
+++ mavu.js
@@ -0,0 +1,25 @@
+const questionInput = document.getElementById('questionInput');
+const askButton = document.getElementById('askButton');
+const answerGif = document.getElementById('answerGif');
+
+askButton.addEventListener('click', function() {
+ const question = questionInput.value;
+ if (!question) {
+  alert('че сидишь? вводи');
+  return;
+ }
+fetch('https://yesno.wtf/api')
+ .then(response => response.json())
+ .then(data => {
+   console.log(data);
+   const answer = data.answer;
+   answerGif.src = data.image;
+   answerGif.style.display=- 'block';
+   questionInput.style.display = 'none';
+   askButton.style.display = 'none';
+ })
+ .catch(error => {
+   console.error ('ты лох, ошибка при запросе к API:', error);
+   alert('вопрос фигня. давай по новой. ');
+  });
+});
