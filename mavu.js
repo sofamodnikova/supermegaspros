76c0380d7278643012773ccca2cc0d15df8ab2c5
sofamodnikova <smodnikova1604@gmail.com>
2026-04-21 13:17:48 +0300

    изменила структуру mavu.js

--- mavu.js
+++ mavu.js
@@ -3,23 +3,23 @@ const askButton = document.getElementById('askButton');
 const answerGif = document.getElementById('answerGif');
 
 askButton.addEventListener('click', function() {
- const question = questionInput.value;
- if (!question) {
-  alert('че сидишь? вводи');
-  return;
- }
-fetch('https://yesno.wtf/api')
- .then(response => response.json())
- .then(data => {
-   console.log(data);
-   const answer = data.answer;
-   answerGif.src = data.image;
-   answerGif.style.display=- 'block';
-   questionInput.style.display = 'none';
-   askButton.style.display = 'none';
- })
- .catch(error => {
-   console.error ('ты лох, ошибка при запросе к API:', error);
-   alert('вопрос фигня. давай по новой. ');
-  });
-});
+    const question = questionInput.value;
+    if (!question) {
+        alert('че сидишь? вводи');
+        return;
+    }
+    fetch('https://yesno.wtf/api')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const answer = data.answer;
+            answerGif.src = data.image;
+            answerGif.style.display=- 'block';
+            questionInput.style.display = 'none';
+            askButton.style.display = 'none';
+    })
+    .catch(error => {
+        console.error ('ты лох, ошибка при запросе к API:', error);
+        alert('вопрос фигня. давай по новой. ');
+    });
+    });
